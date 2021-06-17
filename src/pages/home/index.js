82a4e3a9d24ba0a2ccdb9423a10a71ecd6cd0381
James Hanna <James.Hanna87@gmail.com>
2021-06-17 01:19:58 -0600

    home button stuff

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,21 +1,18 @@
 import React, { useState } from "react";
 import Zoom from "react-reveal/Zoom";
 import { useHistory } from "react-router-dom";
-import leftArrow from "../../images/red-arrow-left-top.png"
-import rightArrow from "../../images/red-arrow-right-top.png"
+import leftArrow from "../../images/red-arrow-left-top.png";
+import rightArrow from "../../images/red-arrow-right-top.png";
 import "./home.css";
 
-
 const Home = ({ darkMode }) => {
-
   const [gotIt, setGotIt] = useState(false);
   const history = useHistory();
 
-
   const gotItHandler = () => {
     setGotIt(!gotIt);
-    history.push("./intro")
-  }
+    history.push("./intro");
+  };
 
   return (
     <div
@@ -26,7 +23,6 @@ const Home = ({ darkMode }) => {
       }}
     >
       <div className="welcome-layover">
-
         <Zoom>
           <div
             style={{
@@ -34,29 +30,35 @@ const Home = ({ darkMode }) => {
               marginTop: "100px",
             }}
           >
-
-
             <div className="welcome-images-container">
+              <div className="welcome-arrow-left-container">
+                <img
+                  src={leftArrow}
+                  alt="left top arrow"
+                  className="leftArrow"
+                />
+                <div className="left-arrow-description-box">
+                  <p>Dark/Light Mode</p>
+                </div>
+              </div>
 
-            <div className="welcome-arrow-left-container">
-              <img src={leftArrow} alt="left top arrow" className="leftArrow" />
-              <div className="left-arrow-description-box">
-                <p>Dark/Light Mode</p>
-                </div> 
+              <div className="welcome-arrow-right-container">
+                <div className="right-arrow-description-box">
+                  <p>Other Color Options</p>
+                </div>
+                <img
+                  src={rightArrow}
+                  alt="right top arrow"
+                  className="rightArrow"
+                />
+              </div>
+
+              <button onClick={() => gotItHandler()} className="got-it-button">
+                Nice Buttons!
+                <br />
+                (continue)
+              </button>
             </div>
-
-            <div className="welcome-arrow-right-container">
-            <div className="right-arrow-description-box">
-                <p>Other Color Options</p>
-                </div> 
-                <img src={rightArrow} alt="right top arrow" className="rightArrow" />
-            </div>
-
-            <button onClick={() => gotItHandler()} className="got-it-button">I Got It</button>
-
-            </div>
-
-
           </div>
         </Zoom>
         <div className="little-space-home"></div>
