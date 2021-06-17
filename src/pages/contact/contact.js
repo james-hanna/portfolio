c0e1cf320c22688b92b90c59650409253be0089d
James Hanna <James.Hanna87@gmail.com>
2021-06-17 00:56:55 -0600

    contact-me section added

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./contact.css";
 import darkModeLogo from "../../images/contact-logo-dark.png";
 import logo from "../../images/contact-logo-light.png";
+import Fade from "react-reveal/Fade";
+import gmail from "../../images/Gmail-icon.png";
+import linked from "../../images/linkedin-icon.png";
+import github from "../../images/GitHub-Mark-32px.png";
 
 const Contact = ({ darkMode }) => {
   return (
@@ -23,11 +27,53 @@ const Contact = ({ darkMode }) => {
           className="title-logo"
           src={darkMode ? darkModeLogo : logo}
           alt="projects pixel"
-        ></img>
+        />
       </div>
-      <div className="little-space-contact">
-        <h3> just testing out the background and scroll!</h3>
-      </div>
+      <Fade top big>
+        <div className="contact-form-container">
+          <form action="https://formspree.io/f/xyybqazz" method="POST">
+            <Fade top big delay={100}>
+              <label for="contact-name">Name:</label>
+            </Fade>
+            <Fade top big delay={300}>
+              <input id="contact-name" type="text" name="name" />
+            </Fade>
+            <Fade top big delay={500}>
+              <label for="contact-email">E-mail:</label>
+            </Fade>
+            <Fade top big delay={700}>
+              <input id="contact-email" type="text" name="email" />
+            </Fade>
+            <Fade top big delay={900}>
+              <label for="leave-a-message">Leave a Suggestion:</label>
+            </Fade>
+            <Fade top big delay={1100}>
+              <textarea id="leave-a-message" name="message"></textarea>
+            </Fade>
+            <Fade top big delay={1300}>
+              <button type="submit">Send!</button>
+            </Fade>
+          </form>
+          <div className="contact-me-btn-container">
+            <Fade top big delay={1500}>
+              <div className="contact-me-btn">
+                <img src={gmail} alt="Gmail icon" onClick={()=> window.open(`mailto:james.hanna87@gmail.com`)}/>
+              </div>
+            </Fade>
+            <Fade top big delay={2100}>
+              <div className="contact-me-btn">
+                <img src={linked} alt="Linked in icon" onClick={()=> window.open(`https://www.linkedin.com/in/james-hanna-dev/`)} />
+              </div>
+            </Fade>
+            <Fade top big delay={1700}>
+              <div className="contact-me-btn">
+                <img src={github} alt="Github icon" onClick={()=> window.open(`https://github.com/james-hanna`)}/>
+              </div>
+            </Fade>
+          </div>
+        </div>
+      </Fade>
+      <div className="little-space-contact"></div>
     </div>
   );
 };
